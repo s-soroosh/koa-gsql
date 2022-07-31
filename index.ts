8b66d981a0660bae69f9cbfbd8965fc4eaa4bd98
Soroosh Sarabadani <soroosh.sarabadani@shopify.com>
2022-07-31 16:45:29 +0200

    add a simple schema

--- index.ts
+++ index.ts
@@ -1,5 +1,25 @@
-import * as Koa from "koa";
-
+import Koa from "koa";
+import mount from "koa-mount";
 const app = new Koa();
+import { buildSchema } from 'graphql';
+
+export const SimpleSchema = buildSchema(`
+type Query {
+    hello: String
+}
+type Subscription {
+    countDown: Int
+}
+`);
+
+import {graphqlHTTP} from "koa-graphql";
+
+app.use(mount(
+    "/graphql",
+    graphqlHTTP({
+        schema: SimpleSchema,
+        graphiql: true
+    })
+))
 
 app.listen(4000);
